119b82c0cab3eda2514606af8068fee40444930e
Miketuazon <michael.d.tuazon@gmail.com>
2023-04-06 16:23:27 -0400

    pushed changes

--- frontend/src/components/PawsRatingInput.js
+++ frontend/src/components/PawsRatingInput.js
@@ -1,12 +1,44 @@
-const PawsRatingInput = ({ rating, disabled, onChange }) => {
+import { useState } from "react";
 
+const PawsRatingInput = ({ rating, disabled, onChange }) => {
+  const [count, setCount] = useState(rating);
   return (
-    <input
-      type="number"
-      disabled={disabled}
-      value={rating}
-      onChange={onChange}
-    />
+    <>
+      <input
+        type="number"
+        disabled={disabled}
+        value={rating}
+        onChange={onChange}
+      />
+      <div className="rating-input">
+        <div className={count < 1 ? "empty" : "filled"}
+          onMouseEnter={(() => setCount(1))}
+          onMouseLeave={(() => setCount(rating))}>
+          <i className="fa fa-paw"></i>
+        </div>
+        <div className={count < 2 ? "empty" : "filled"}
+          onMouseEnter={(() => setCount(2))}
+          onMouseLeave={(() => setCount(rating))}>
+
+          <i className="fa fa-paw"></i>
+        </div>
+        <div className={count < 3 ? "empty" : "filled"}
+          onMouseEnter={(() => setCount(3))}
+          onMouseLeave={(() => setCount(rating))}>
+          <i className="fa fa-paw"></i>
+        </div>
+        <div className={count < 4 ? "empty" : "filled"}
+          onMouseEnter={(() => setCount(4))}
+          onMouseLeave={(() => setCount(rating))}>
+          <i className="fa fa-paw"></i>
+        </div>
+        <div className={count < 5 ? "empty" : "filled"}
+          onMouseEnter={(() => setCount(5))}
+          onMouseLeave={(() => setCount(rating))}>
+          <i className="fa fa-paw"></i>
+        </div>
+      </div>
+    </>
   );
 };
 
